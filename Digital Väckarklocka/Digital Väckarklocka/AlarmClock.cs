820029056da7c4a6d88958c508f77f493d226d9f
Mattias Wikström <mw222rs@student.lnu.se>
2014-10-04 16:09:07 +0200

    börjat så smått

---
+++ Digital Väckarklocka/Digital Väckarklocka/AlarmClock.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Väckarklocka
+{
+    public class AlarmClock
+    {
+        private int _alarmHour;
+        private int _alarmMinute;
+
+        private int _hour;
+        private int _minute;
+
+        public int AlarmHour
+        {
+            get { return _alarmHour; }
+            set
+            {
+                if (value < 0 || value > 23)
+                {
+                    throw new ArgumentException();                    
+                }
+                _alarmHour = value;
+            }
+        }
+
+        public int AlarmMinute
+        {
+            get { return _alarmMinute; }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException();                    
+                }
+                _alarmMinute = value;
+            }
+        }
+
+        public int Hour
+        {
+            get { return _hour; }
+            set
+            {
+                if (value < 0 || value > 23)
+                {
+                    throw new ArgumentException();
+                }
+                _hour = value;
+            }
+        }
+
+        public int Minute
+        {
+            get { return _minute; }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException();
+                }
+                _minute = value;
+            }
+        }
+
+        public AlarmClock()
+        {
+            // Tom!
+        }
+
+        public AlarmClock(int hour, int minute)
+        {
+            Hour = hour;
+            Minute = minute;
+        }
+
+        public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
+        {
+            Hour = hour;
+            Minute = minute;
+            AlarmHour = alarmHour;
+            AlarmMinute = alarmMinute;
+        }
+
+        public bool TickTock()
+        {
+            return true;
+        }
+
+        public string ToString()
+        {
+            return "Hello!";
+        }
+    }
+}
---
+++ Digital Väckarklocka/Digital Väckarklocka/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Väckarklocka
+{
+    class Program
+    {
+        private string HorizontalLine = "Hej en linje.";
+
+        static void Main(string[] args)
+        {
+            AlarmClock klocka = new AlarmClock(22, 55, 21, 20);
+
+            Console.WriteLine("{0} : {1}, {2} : {3}", klocka.Hour, klocka.Minute, klocka.AlarmHour, klocka.AlarmMinute);
+
+        }
+
+        private static void Run(AlarmClock ac, int minutes)
+        {
+            // VA?!
+        }
+
+        private static void ViewErrorMessage(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        private static void ViewTestHeader(string header)
+        {            
+            Console.WriteLine();
+            Console.WriteLine(header);
+            Console.WriteLine();
+        }
+    }
+}
